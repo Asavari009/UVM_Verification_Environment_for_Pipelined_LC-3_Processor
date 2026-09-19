81ec44f475780641005de3f0460b058efa19ebb8
Asavari009 <asavari2002@gmail.com>
2026-09-19 13:29:20 -0400

    Create decode_test_pkg.sv

---
+++ project_benches/decode/tb/tests/decode_test_pkg/decode_test_pkg.sv
@@ -0,0 +1,14 @@
+//-----------------------------------------------------------------------------
+// decode_test_pkg.sv
+//-----------------------------------------------------------------------------
+package decode_test_pkg;
+
+  // loading all the packages that are required
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+
+  import decode_in_pkg::*;
+
+  `include "test_top.sv"
+
+endpackage : decode_test_pkg
